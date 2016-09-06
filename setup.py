d39b4f7c25d669b9097021054ed678f5ded49d2b
jamesstidard <jamesstidard@gmail.com>
2016-09-06 19:01:05 +0100

    load config

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 from setuptools import find_packages
-from pip.req import parse_requirements as parse
 
 
 setup(
     name='talka2z',
-    version='0.4',
+    version='0.5',
     packages=find_packages(exclude=('tests', 'tests.*')),
     install_requires=[
     ]
